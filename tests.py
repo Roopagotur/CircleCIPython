7abebb9c94e304fb6af73fb55c11b6281fa0b5b8
Roopa G <roopa.g@thinkbridge.in>
2025-02-20 15:46:52 +0530

    Name changed in test

--- tests.py
+++ tests.py
@@ -6,7 +6,7 @@ class MyTestCase(unittest.TestCase):
     def test_to_upper(self):
         name = "RoopaGotur"
         upper_name = to_upper(name)
-        self.assertEqual(upper_name, "ROOPA")
+        self.assertEqual(upper_name, "RoopaGotur")
 
 
 if __name__ == '__main__':
